85e193c9c14e925946efd6ad5bfb8ce0837973f2
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-06-19 19:00:24 +0900

    不要ｎふぁいるを削除

--- pandasetutils/print_semseg_count.py
+++ pandasetutils/print_semseg_count.py
@@ -1,77 +1,114 @@
+import argparse
+import copy
 import logging
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from collections import Counter
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 import pandas
-from panda2anno.common.utils import set_default_logger
 from pandaset import DataSet
-from pandaset.sequence import Sequence
+
+from pandasetutils.common.utils import set_logger
 
 logger = logging.getLogger(__name__)
 
 
-def parse_args():
-    parser = ArgumentParser(
-        description="PandaSetのcuboidのlabel数を出力します。",
-        formatter_class=ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument("-i", "--input_dir", type=Path, required=True, help="pandasetのディレクトリ")
-    parser.add_argument("-o", "--output", type=Path, required=True, help="出力先")
-
-    parser.add_argument("--sequence_id", type=str, nargs="+", required=False, help="出力対象のsequence id")
-
-    return parser.parse_args()
+@dataclass(frozen=True)
+class SemsegCounts:
+    sequence_id: str
+    frame_no: int
+    point_counts: dict[str, int]
+    """keyがlabel, valueが点の個数であるdict"""
 
 
-def get_label_counter(sequence: Sequence) -> dict[str, int]:
-    sequence.load_cuboids()
-    # 先頭だけ見る
-    df = sequence.semseg.data[0]
-    tmp = Counter(df["class"])
-    classes = sequence.semseg.classes
-    return {classes[str(class_id)]: count for class_id, count in tmp.items()}
+class DataSetAccessor:
+    def __init__(self, dataset: DataSet) -> None:
+        self.dataset = dataset
+
+    def get_semseg_counts_list(self, sequence_id: str) -> list[SemsegCounts]:
+        """
+
+        keyは
+        """
+        sequence = self.dataset[sequence_id]
+        # sequence.load_cuboids()
+
+        result: list[SemsegCounts] = []
+        try:
+            for frame_no, df in enumerate(sequence.semseg.data):
+                counter = Counter(df["class"])
+                result.append(SemsegCounts(sequence_id=sequence_id, frame_no=frame_no, counts=counter))
+        finally:
+            self.dataset.unload(sequence_id)
+
+        return result
+
+
+def create_semseg_point_counts_dataframe(input_dir: Path, sequence_id_list: None | list[str] = None) -> pandas.DataFrame:
+    """
+    labelごとのsemsegの点の個数が格納された`pandas.DataFrame`を生成します。
+
+    Args:
+        pandasetのディレクトリ
+        sequence_id_list: 取得対象のsequence_idのlist。Noneならば全てのsequence_idが取得対象になります。
+
+    Returns:
+        index: sequence_id, frame_no
+        columns: cuboidのlabel
+    """
+    dataset = DataSet(str(input_dir))
+    _sequence_id_list = sequence_id_list if sequence_id_list is not None else dataset.sequences()
+
+    dataset_accessor = DataSetAccessor(dataset)
+
+    data: list[dict[str, Any]] = []
+    for sequence_id in _sequence_id_list:
+        logger.debug(f"{sequence_id=} :: semsegのlabelごとの点の個数を取得します。")
+
+        try:
+            semseg_counts_list = dataset_accessor.get_semseg_counts_list(sequence_id)
+            for semseg_counts in semseg_counts_list:
+                tmp: dict[str, Any] = copy.deepcopy(semseg_counts.point_counts)
+                tmp["frame_no"] = semseg_counts.frame_no
+                tmp["sequence_id"] = semseg_counts.sequence_id
+                data.append(tmp)
+        except Exception:
+            logger.warning(f"{sequence_id=} :: labelごとのsemsegの点の個数の取得に失敗しました。", exc_info=True)
+            continue
+
+    df = pandas.DataFrame(data)
+    df = df.fillna(0)
+    df = df.set_index(["sequence_id", "frame_no"])
+
+    # columnを辞書順に並び替える
+    df = df[sorted(df.columns)]
+    return df
 
 
 def main() -> None:
     args = parse_args()
-    set_default_logger()
+    set_logger()
 
-    input_dir: Path = args.input_dir
+    df = create_semseg_point_counts_dataframe(args.input_dir, args.sequence_id)
+    output_file = args.output
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+    df.to_csv(str(output_file))
 
-    dataset = DataSet(str(input_dir))
 
-    if args.sequence_id is None:
-        sequence_id_list = dataset.sequences()
-    else:
-        sequence_id_list = args.sequence_id
+def parse_args() -> argparse.Namespace:
+    parser = ArgumentParser(
+        description="semsegのlabelごとの点数をCSV形式で出力します。",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-i", "--input_dir", type=Path, required=True, help="pandasetのディレクトリ")
+    parser.add_argument("-o", "--output", type=Path, required=True, help="CSVファイルの出力先")
+    parser.add_argument(
+        "--sequence_id", type=str, nargs="+", required=False, help="集計対象のsequence id。未指定ならばすべてのsequence idが対象です。"
+    )
 
-    data: list[dict[str, Any]] = []
-    for sequence_id in sequence_id_list:
-        sequence = dataset[sequence_id]
-        logger.info(f"{sequence_id=}のsemsegのlabel一覧を取得します。")
-        try:
-            sequence.load_semseg()
-            tmp = get_label_counter(sequence)
-            tmp["sequence_id"] = sequence_id
-            data.append(tmp)
-
-        except Exception:
-            logger.warning(f"{sequence_id=}のcuboidのロードに失敗しました。", exc_info=True)
-        finally:
-            dataset.unload(sequence_id)
-
-    df = pandas.DataFrame(data)
-    df = df.fillna(0)
-
-    tmp_columns = set(df.columns)
-    columns = ["sequence_id", *list(tmp_columns - {"sequence_id"})]
-
-    output: Path = args.output
-    output.parent.mkdir(exist_ok=True, parents=True)
-
-    df[columns].to_csv(str(output), index=False)
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
