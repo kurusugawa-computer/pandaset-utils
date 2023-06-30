ca2c77b4d689a2b7b1d98e7b1c07cb67aa998b5a
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-06-30 11:14:30 +0900

    devcontainerが動くように修正 (#6)
    * devcontainerが動くように修正
    
    * 不要なマウントを削除する
    
    * format
    
    * format

--- .devcontainer/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-# docker.sock から gid を取得して、docker グループの gid を変更
-docker_group_id=$(stat /var/run/docker.sock --format="%g")
-sudo groupmod --gid ${docker_group_id} docker
-
-# 無限待ち（vscode デフォルトの entrypoint と同じ方法で）
-while sleep 1000; do :; done
